04186903f81af2feaa771567958b9f7f83388807
Wakwaw <114848886+Wakwawman@users.noreply.github.com>
2025-03-23 23:50:54 +0700

    Mengenal Controller, Function, Route & View

---
+++ Controllers/HelloWorldController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloWorldController extends Controller
+{
+    //
+    public function index()
+    {
+        return "Selamat Belajar Framework Laravel 10";
+    }
+    public function ambilfile()
+    {
+        return view('ambilfile');
+    }
+
+}
---
+++ views/v_html/getlorem.blade.php
@@ -0,0 +1,9 @@
+<p>
+    <b>Mengenal Huruf Tebal</b><br> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure reprehenderit expedita nostrum rem dicta itaque perferendis corporis officia ipsum? Ab nesciunt veritatis accusantium exercitationem pariatur libero blanditiis sunt minus quas?
+</p>
+<p>
+    <u>Mengenal Huruf Garis Bawah</u><br> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga tempora quis repellat odio, doloribus laudantium corporis saepe porro praesentium accusamus voluptatibus soluta voluptatem numquam? Molestias quo perferendis impedit maxime illo?
+</p>
+<p>
+    <i>Mengenal Huruf Miring</i><br> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga dolorem expedita aperiam perferendis incidunt unde autem ipsum cum! Eum obcaecati veritatis illum pariatur ex corporis aliquid dignissimos repellat eveniet sunt.
+</p>
